62452d693e1b405a761762cc8011fd9fc6aaa234
Maria Lissarrague <31631513+meryliss@users.noreply.github.com>
2022-12-20 11:50:25 -0300

    Add files via upload
    Update REST code
    Included WebSocket Code

--- ProyectoAlmaviaREST.py
+++ ProyectoAlmaviaREST.py
@@ -13,23 +13,33 @@ import yfinance as yf
 import numpy as np
 import datetime
 import unittest
+import sys
 
-rofex_user = "mhlissarrague7682"
-rofex_password = "objxhQ1#"
-rofex_account = "REM7682"
+#Instruments to test given as a dictionary
+#Key is the Yahoo Finance Ticker
+#Value is the ROFEX ticker of the future contract to be tested
 
-
-instruments = {"YPFD.BA":"YPFD/FEB23", "GGAL.BA": "GGAL/FEB23",
-                "PAMP.BA": "PAMP/FEB23","ARS=X" : "DLR/FEB23"}
-
-
-
+instruments = {"PAMP.BA" : "PAMP/FEB23"}
+    
+#Remarkets account credentials
+remarkets_user = "mhlissarrague7682"
+remarkets_password = "objxhQ1#"
+remarkets_account = "REM7682"    
+       
 class Implied_rates:
-    def __init__(self, fut_price, spot_price, tte):
+    def __init__(self, fut_price, spot_price, currency,tte):
+        #Object that contains the impormation for calculating implied rate
+        #Parameters
+        #fut_price : Price of the future intrument (float)
+        #spo_price : Spot price of the underlying (float)
+        #currency : currency in which both spot and future price is given (str)
+        #tte : Time to expiry of the future instrument in fraction of years (float)
         self.future_price = fut_price
         self.spot_price = spot_price
+        self.currency = currency
         self.tte = tte
     def calc_rate(self):
+        #returns implied rate in relative value
         x = self.future_price/self.spot_price
         y = 1/self.tte
         implied_rate = np.power(x,y) - 1 
@@ -37,36 +47,55 @@ class Implied_rates:
 
 
 class TestCalcImpliedRates(unittest.TestCase):
+    #checks wether the implied rate is calculated corrctly
     def runTest(self):
-        ImpRate = Implied_rates(50,40,1)
+        ImpRate = Implied_rates(50,40,"ARS", 1)
         self.assertEqual(ImpRate.calc_rate(), 0.25, "incorrect value")
 
 unittest.main()
 
 
 if __name__ == '__main__' :
+
+    # Initialize Remarket account
+    pr.initialize(user = remarkets_user, password = remarkets_password,
+                    account=remarkets_account,environment=pr.Environment.REMARKET)  
     
-    pr.initialize(user = rofex_user, password = rofex_password,
-                    account=rofex_account,environment=pr.Environment.REMARKET)  
+    # Get and store the initial prices for future intruments and the corresponding underlyers in 
+    # order to check for future changes in prices.
+    # If there is no initial information for the selected instruments the code exits and indicates 
+    # the absence of information.
     
     instrument_info = {}
     
     for i,j in instruments.items():
         
         fut_info = pr.get_market_data(ticker = j)
+        
 
         mat_date = datetime.datetime.strptime(pr.get_instrument_details(ticker = j)
                                         ["instrument"]["maturityDate"], "%Y%m%d").date()
     
-        stock_info = yf.Ticker(i)
+        try:
+            fut_price_bid_init = fut_info["marketData"]["BI"][0]["price"]
+        
+            fut_price_ask_init = fut_info["marketData"]["OF"][0]["price"]
+            
+            stock_info = yf.Ticker(i)
          
-        stock_price_init = stock_info.info['regularMarketPrice']
+            stock_price_init = stock_info.info['regularMarketPrice']
         
-        data = {"FutName" : j, "MatDate" : mat_date,"StockPrice" : stock_price_init }
-        
-        instrument_info[ i ] = data
+            data = {"FutName" : j, "FutBidPrice" :fut_price_bid_init, "FutAskPrice" :fut_price_ask_init,
+                "MatDate" : mat_date,"StockPrice" : stock_price_init }
+            
+            instrument_info[ i ] = data
+        except:
+            print("No available initial data for " + j)
+            
+            sys.exit()
 
-    
+    print(instrument_info)
+
     while True:
         
         for i in instrument_info.keys():
@@ -74,37 +103,61 @@ if __name__ == '__main__' :
             asset_data = instrument_info [ i ]
             
             try:
+                
+                # for each of the listed instruments system gets the current price
                 stock_info_now = yf.Ticker(i)
+                
                 stock_price_curr = stock_info_now.info['regularMarketPrice']
+                
+                fut_info_now = pr.get_market_data(ticker=asset_data["FutName"])
+                
+                fut_price_bid = fut_info_now["marketData"]["BI"][0]["price"]
+                
+                fut_price_ask = fut_info_now["marketData"]["OF"][0]["price"]
+                
+                #compares current price with the one currently stored, if there is a price change
 
                 try:
-                    if stock_price_curr != asset_data["StockPrice"]:
+                    #compares current price with the one currently stored
+                    
+                    if stock_price_curr != asset_data["StockPrice"] or fut_price_bid != asset_data["FutBidPrice"] or fut_price_ask!= asset_data["FutAskPrice"] :
+                        
+                        #if there is price change, system calculates implied rates
                         
                         today = datetime.datetime.today()
                         
                         tte = (asset_data["MatDate"] - today.date()).days/365
-                    
-                        fut_info_now = pr.get_market_data(ticker=asset_data["FutName"])
                 
-
-                        fut_price_bid = fut_info_now["marketData"]["BI"][0]["price"]
-                        fut_price_ask = fut_info_now["marketData"]["OF"][0]["price"]
-                
-                        imp_rate_bid = Implied_rates(fut_price_bid, stock_price_curr, tte).calc_rate()
-                        imp_rate_ask = Implied_rates(fut_price_ask, stock_price_curr, tte).calc_rate()
+                        imp_rate_bid = Implied_rates(fut_price_bid, stock_price_curr, "ARS", tte).calc_rate()
+                        
+                        imp_rate_ask = Implied_rates(fut_price_ask, stock_price_curr, "ARS", tte).calc_rate()
             
                         print (i + " Spot Price Change")
                         print ("Bid Implied Rate: " + str (round(imp_rate_bid*100,3)) + " %")
                         print ("Ask Implied Rate: " + str (round(imp_rate_ask*100,3)) + " %")
                         
+                        #system check for arbitrage by comapring bid/ask implied rates
+                        
                         if imp_rate_ask < imp_rate_bid:
                             print(" THERE IS ARBITRAGE OPPORTUNITY")
+                        else:
+                            print("NO ARBITRAGE OPPORTUNITY")
+                        
+                        #update prices for comparison
             
                         asset_data ["StockPrice"] = stock_price_curr
+                        asset_data ["FutBidPrice"] = fut_price_bid
+                        asset_data ["FutAskPrice"] = fut_price_ask
                 except:
-                    pass 
+                    if KeyboardInterrupt():
+                        sys.exit()
+                    else:
+                        pass 
             except:
-                pass
+                if KeyboardInterrupt():
+                    sys.exit()
+                else:
+                    pass
             
             print(".")
             
